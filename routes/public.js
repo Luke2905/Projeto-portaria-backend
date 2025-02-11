4ee574e2651642ae2a4adc1dc47192813fa9faed
Lucas Laranjeira <88212940+Luke2905@users.noreply.github.com>
2025-02-11 15:20:50 -0300

    Criação das rotas publicas

---
+++ routes/public.js
@@ -0,0 +1,62 @@
+import express, { request, response } from 'express' // -> Biblioteca do express
+import { PrismaClient } from '@prisma/client'
+import cors from 'cors'
+import bcrypt from 'bcrypt'
+
+
+const prisma = new PrismaClient() // -> Conexão com o prisma
+
+const PublicRoutes = express.Router() //-> Router: função do Express para roteamento
+
+PublicRoutes.use(express.json()) //-> indica para o express que ele vai receber os dados via JSON
+
+
+PublicRoutes.use(cors()) //-> Configura o backend para aceitar a requisição do frontend
+
+
+/*----------------------- Cadastro de Usuario --------------------  */
+PublicRoutes.post('/cadastro', async (request, response) => { //-> para usar await a função dece ser async
+
+    try{ //-> é a condicional de verificação de execução (tentar), caso de erro cai no catch
+
+        const user = request.body //-> variavel para requisição
+
+        const salt = await bcrypt.genSalt(10) //-> Variavel do Bcryp para definir o pesso da incriptação
+
+        const hashPassword = await bcrypt.hash(user.password, salt) //-> pega a senha e gera o hash
+
+        const userDB =  await prisma.users.create({
+    
+            data:{
+              name:  user.name,
+              user:  user.user,
+              email:  user.email,
+              password: hashPassword,
+              acessos:  user.acessos
+            },
+        })
+        response.status(201).json(userDB)
+
+    }catch(err){
+
+        response.status(500).json({message: 'Erro no servidor, tente novamente!'})
+
+    }
+
+})
+
+/*-------------------------------- Fim Cadastro Usuario -------------------------------- */
+
+/*----------------------- Função para listar os usuarios cadastrados --------------------------- */
+
+PublicRoutes.get('/usuarios', async (request, response) => {
+
+    const usuarios = await prisma.users.findMany()
+
+    response.status(200).json(usuarios) // -> retorna a requisição /*-> 200 responde o status da requisição para o usuario */
+
+})  //GET -> metodo usado para exibir/listar 
+
+/*-------------------------------------- X ------------------------------------------- */
+
+export default PublicRoutes
